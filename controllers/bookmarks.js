d6a650e9b624b4b8fdcfb611b920d265be4b5cea
f.kresna23 <f.kresna23@gmail.com>
2021-11-11 20:57:12 +0700

    Update Bookmark dan Comment v1

---
+++ controllers/bookmarks.js
@@ -0,0 +1,161 @@
+const { bookmark, events, users } = require("../models");
+
+class Bookmark {
+  // get all bookmark
+  async getAllBookmark(req, res, next) {
+    try {
+      let data = await bookmark.findAll({
+        // find all data in table bookmark
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   include is join
+          {
+            model: events,
+            include: [
+              //   include is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+
+      // If there is nothing here
+      if ((data, length === 0)) {
+        return res.status(404).json({ errors: ["Bookmark not found"] });
+      }
+      // If success
+      res.status(200).json({ data });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+  async getDetailBookmark(req, res, next) {
+    try {
+      let data = await bookmark.findOne({
+        // find all data of bookmark table
+        where: { id: req.params.id },
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+
+      // if theres nothing here
+      if (!data) {
+        return res.status(404).json({ errors: ["Bookmark is not found"] });
+      }
+
+      // if success
+      res.status(200).json({ data });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+
+  async createBookmark(req, res, next) {
+    try {
+      // create bookmark
+      const newData = await bookmark.create(req.body);
+
+      // find bookmark with join
+      const data = await bookmark.findOne({
+        where: {
+          id: newData.id,
+        },
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+      res.status(201).json({ data });
+    } catch (error) {
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+
+  // update data
+  async updateBookmark(req, res, next) {
+    try {
+      // bookmark table update
+      const updateData = await bookmark.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      // if no data updated
+      if (updatedData[0] === 0) {
+        return res.status(404).json({ errors: ["Bookmark not found"] });
+      }
+
+      // find the updated bookmark
+      const data = await transaction.findOne({
+        where: {
+          id: req.params.id,
+        },
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+
+      // if success
+      res.status(201).json({ data });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+
+  // delete data
+  async deleteBookmark(req, res, next) {
+    try {
+      // delete data
+      let data = await bookmark.destroy({ where: { id: req.params.id } });
+
+      // If data deleted is null
+      if (!data) {
+        return res.status(404).json({ errors: ["Bookmark not found"] });
+      }
+
+      // if success
+      res.status(200).json({ message: "Success delete bookmark" });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+}
+
+module.exports = new Bookmark();
---
+++ controllers/comments.js
@@ -0,0 +1,159 @@
+const { comment, events, users } = require("../models");
+
+class Comment {
+  // Get all comment
+  async getAllComment(req, res, next) {
+    try {
+      let data = await comment.findAll({
+        // find all data in table comment
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+      // if there is no data in comment
+      if (data.length === 0) {
+        return res.status(404).json({ errors: ["Comment not found"] });
+      }
+      // if success
+      res.status(200).json({ data });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+  // get detail comment
+  async getDetailComment(req, res, next) {
+    try {
+      let data = await comment.findOne({
+        // find all data in comment table
+        where: { id: req.params.id },
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+
+      // if there is no data
+      if (!data) {
+        return res.status(404).json({ errors: ["Comment not found"] });
+      }
+
+      // if success
+      res.status(200).json({ data });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+  // Create comment
+  async createComment(req, res, next) {
+    try {
+      // create comment
+      const newData = await comment.create(req.body);
+
+      // find event with join
+      const data = await comment.findOne({
+        where: {
+          id: newData.id,
+        },
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+
+      res.status(201).json({ data });
+    } catch (error) {
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+
+  // Update data
+  async updateComment(req, res, next) {
+    try {
+      // transaction table update data
+      const updatedData = await transaction.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      // If no data updated
+      if (updatedData[0] === 0) {
+        return res.status(404).json({ errors: ["Comment not found"] });
+      }
+      // find the updated comment
+      const data = await comment.findOne({
+        where: {
+          id: req.params.id,
+        },
+        attributes: {
+          exclude: ["id_event", "id_user", "createdAt", "updatedAt"],
+        },
+        include: [
+          //   includes is join
+          {
+            model: events,
+            include: [
+              // includes is join
+              { model: users },
+            ],
+          },
+        ],
+      });
+      // if success
+      res.status(201).json({ data });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+
+  // Delete data
+  async deleteComment(req, res, next) {
+    try {
+      // delete data
+      let data = await comment.destroy({ where: { id: req.params.id } });
+      // if delete data is null
+      if (!data) {
+        return res.status(404).json({ errors: ["Comment not found"] });
+      }
+
+      // if success
+      res.status(200).json({ message: "Success delete comment" });
+    } catch (error) {
+      // if error
+      res.status(500).json({ errors: ["Internal Server Error"] });
+    }
+  }
+}
+
+module.exports = new Comment();
