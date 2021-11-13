bc0bc6f495d8a30b47b393c110378d52d426dd05
f.kresna23 <f.kresna23@gmail.com>
2021-11-13 10:08:34 +0700

    Revisi routes bookmark dan comments

--- routes/comments.js
+++ routes/comments.js
@@ -18,13 +18,13 @@ const router = express.Router();
 
 // it will find route that has / first. after that it, will find is it GET or POST
 router
-  .router("/")
+  .route("/")
   .get(getAllComment)
   .post(createOrUpdateCommentValidator, createComment);
 
 // it will find route that has /: id first. after that, it will find is it GET or PUT or DELETE
 router
-  .router("/:id")
+  .route("/:id")
   .get(getDetailComment)
   .put(createOrUpdateCommentValidator, updateComment)
   .delete(deleteComment);
