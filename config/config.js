6c37ce9270f6167979d5d2f6c4f21dc889125f51
Nushair Falah <lendsham1st@gmail.com>
2021-11-17 01:33:18 +0700

    deploy to heroku fix

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
@@ -15,10 +16,15 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: "rlboinubdojyvi",
+    password: "dbd11861c82fbc230ba7302c6aacdc1b1d651f427a992a540904f478be1984e6",
+    database: "d1pu4g94drfqvh",
+    host: "ec2-3-230-149-158.compute-1.amazonaws.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
