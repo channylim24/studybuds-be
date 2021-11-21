aaeee570f6c43d5588cc20db80214e6a9fca8e77
Nushair Falah <lendsham1st@gmail.com>
2021-11-21 19:28:05 +0700

    test 2

--- test/users.test.js
+++ test/users.test.js
@@ -7,103 +7,103 @@ const { encrypt, decrypt, verifyToken, createToken } = require("../utils");
 const hashedPass = encrypt("testTest123*");
 console.log(hashedPass, "<<<<< HASHED");
 
-beforeAll(async () => {});
+beforeAll(async () => { });
 
 // afterAll(async () => {
 //   await queryInterface.bulkDelete("users");
 // });
 
 describe("Register", () => {
-  test("Register success", (done) => {
-    request(app)
-      .post("/user/register")
-      .send({
-        firstName: "Daniel",
-        lastName: "Doe",
-        email: "daniel@example.com",
-        password: hashedPass,
-      })
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        }
+    test("Register success", (done) => {
+        request(app)
+            .post("/user/register")
+            .send({
+                firstName: "Daniel",
+                lastName: "Doe",
+                email: "daniel@mail.com",
+                password: hashedPass,
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
 
-        const { body, status } = res;
-        console.log(body, "<<<<< BODY");
-        expect(status).toBe(201);
-        expect(body).toHaveProperty("data");
-        expect(body.data).toHaveProperty("firstName", "Daniel");
-        expect(body.data).toHaveProperty("lastName", "Doe");
-        expect(body.data).toHaveProperty("email", "daniel@example.com");
-        done();
-      });
-  });
-  // apabila firstName kosong/null
-  test("Register not success", (done) => {
-    request(app)
-      .post("/user/register")
-      .send({
-        firstName: "",
-        lastName: "Doe",
-        email: "daniel@example.com",
-        password: hashedPass,
-      })
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        }
+                const { body, status } = res;
+                console.log(body, "<<<<< BODY");
+                expect(status).toBe(201);
+                expect(body).toHaveProperty("data");
+                expect(body.data).toHaveProperty("firstName", "Daniel");
+                expect(body.data).toHaveProperty("lastName", "Doe");
+                expect(body.data).toHaveProperty("email", "daniel@mail.com");
+                done();
+            });
+    });
+    // apabila firstName kosong/null
+    test("Register not success", (done) => {
+        request(app)
+            .post("/user/register")
+            .send({
+                firstName: "",
+                lastName: "Doe",
+                email: "daniel@mail.com",
+                password: hashedPass,
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
 
-        const { body, status } = res;
-        console.log(body, "<<<<< BODY");
-        expect(status).toBe(400);
-        done();
-      });
-  });
-  // apabila email tidak sesuai dengan kaidah email yang benar
-  test("Register not success", (done) => {
-    request(app)
-      .post("/user/register")
-      .send({
-        firstName: "Daniel",
-        lastName: "Doe",
-        email: "daniel@example.haha",
-        password: hashedPass,
-      })
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        }
+                const { body, status } = res;
+                console.log(body, "<<<<< BODY");
+                expect(status).toBe(400);
+                done();
+            });
+    });
+    // apabila email tidak sesuai dengan kaidah email yang benar
+    test("Register not success", (done) => {
+        request(app)
+            .post("/user/register")
+            .send({
+                firstName: "Daniel",
+                lastName: "Doe",
+                email: "daniel@mail.haha",
+                password: hashedPass,
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
 
-        const { body, status } = res;
-        console.log(body, "<<<<< BODY");
-        expect(status).toBe(400);
-        done();
-      });
-  });
-  // apabila password tidak mengandung salah satu dari :
-  // 1. huruf kecil
-  // 2. huruf kapital
-  // 3. special character
-  // 4. angka
-  test("Register not success", (done) => {
-    const wrongPass = encrypt("123456");
-    request(app)
-      .post("/user/register")
-      .send({
-        firstName: "Daniel",
-        lastName: "Doe",
-        email: "daniel@example.haha",
-        password: wrongPass,
-      })
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        }
+                const { body, status } = res;
+                console.log(body, "<<<<< BODY");
+                expect(status).toBe(400);
+                done();
+            });
+    });
+    // apabila password tidak mengandung salah satu dari :
+    // 1. huruf kecil
+    // 2. huruf kapital
+    // 3. special character
+    // 4. angka
+    test("Register not success", (done) => {
+        const wrongPass = encrypt("123456");
+        request(app)
+            .post("/user/register")
+            .send({
+                firstName: "Daniel",
+                lastName: "Doe",
+                email: "daniel@mail.haha",
+                password: wrongPass,
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
 
-        const { body, status } = res;
-        console.log(body, "<<<<< BODY");
-        expect(status).toBe(400);
-        done();
-      });
-  });
+                const { body, status } = res;
+                console.log(body, "<<<<< BODY");
+                expect(status).toBe(400);
+                done();
+            });
+    });
 });
