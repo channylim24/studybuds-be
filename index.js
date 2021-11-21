f2a153ad27805fe6cb0ed5cb0a5b734fdb655983
f.kresna23 <f.kresna23@gmail.com>
2021-11-21 17:13:44 +0700

    CI-CD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express"); // import express
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
-const http = require('http')
+const http = require("http");
 
 // import routes
 const events = require("./routes/events");
@@ -11,17 +11,17 @@ const users = require("./routes/users");
 const categorys = require("./routes/categorys");
 const bookmarks = require("./routes/bookmarks");
 const comments = require("./routes/comments");
-const login = require('./routes/login');
+const login = require("./routes/login");
 
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 // enable req.body (json and urlencoded)
@@ -30,35 +30,38 @@ app.use(express.urlencoded({ extended: true }));
 
 // ---- CORS setting
 const corsOptions = {
-    origin: '*',
-    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: "*",
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Content-Length",
+    "X-Requested-With",
+    "Accept",
+  ],
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
 
 // Enable req.body (form-data)
 app.use(fileUpload());
 
-app.use(express.static('public'));
-
-
+app.use(express.static("public"));
 
 // make routes
 app.use("/user", users);
-app.use('/login', login);
+app.use("/login", login);
 app.use("/event", events);
 app.use("/category", categorys);
 app.use("/comment", comments);
 app.use("/bookmark", bookmarks);
 
-
 app.all("*", function (req, res) {
-    res.status(404)
-    res.end(JSON.stringify({ message: "Endpoint does not exist" }))
+  res.status(404);
+  res.end(JSON.stringify({ message: "Endpoint does not exist" }));
 });
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 // run the server
 const port = process.env.PORT || 3000; // define port
